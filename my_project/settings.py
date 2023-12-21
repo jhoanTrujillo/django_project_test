6892f9aa42a10487d7b94e2d732c8a9230b9fea6
jhoanTrujillo <jhoantrujillo95@gmail.com>
2023-12-21 09:25:53 +0000

    fix (settings.py): added herokuapp.com to allowed_host

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=@tk(r3=r0d%(ckj!+g+kc7q@j^554b)xup^!hie(s$)ii8w=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
